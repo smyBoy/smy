74e36d481149a8c2c0765d19c01769a352324bf5
smyBoy <1024558667@qq.com>
2018-07-24 08:39:17 +0800

    init

--- start-web/src/test/java/com/smy/test/user/UserDao.java
+++ start-web/src/test/java/com/smy/test/user/UserDao.java
@@ -28,13 +28,13 @@ public class UserDao {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<User> query = builder.createQuery(User.class);
         Root<User> userRoot = query.from(User.class);
-        Root<Org> orgRoot = query.from(Org.class);
         query.select(userRoot);
         List<Predicate> pList = new ArrayList<>();
         if (!StringUtils.isEmpty(username)) {
             pList.add(WhereUtil.createWhere(builder, userRoot, WhereData.le("name", username)));
         }
         if (StringUtils.isEmpty(orgName)) {
+            Root<Org> orgRoot = query.from(Org.class);
             pList.add(WhereUtil.createWhere(builder, orgRoot, WhereData.le("name", orgName)));
             pList.add(builder.equal(userRoot.get("orgId"), orgRoot.get("id")));
         }
